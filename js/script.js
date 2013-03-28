7ed3e69cdbc95a2949a43fd430dba4da6338bca2
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-03-28 14:09:26 +0100

    on ne dit pas teube, mais teu.be svp

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@
 		var board = new DrawingBoard.Board('teuboard', {
 			controls: [{ Color: {defaultColor: "rgba(255, 191, 127, 1)"}}, 'Size', 'Navigation']
 		});
+	$('.teu').each(function(key, item) {
+		console.log(item.innerHTML);
+		item.innerHTML = item.innerHTML.replace(/teube/, 'teu<span class="be">be</span>');
+	});
 })();
--- views/elements/add.php
+++ views/elements/add.php
@@ -1,12 +1,12 @@
-<form class="teube teube-add" action="<?php echo $app->urlFor('add'); ?>" method="post">
+<form class="teube-add" action="<?php echo $app->urlFor('add'); ?>" method="post">
 	<div id="teuboard">
-		
+
 	</div>
 
 	<div class="inputs">
-		<label for="teube-name">Nom de la teube</label>
+		<label for="teube-name" class="teu">Nom de la teube</label>
 		<input type="text" id="teube-name" name="name">
-		
-		<button>Je valide ma teube</button>
+
+		<button class="teu">Je valide ma teube</button>
 	</div>
 </form>
