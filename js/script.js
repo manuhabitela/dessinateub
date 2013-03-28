e45c79edc81c403fea61d93311dee825776baddf
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-03-28 14:10:21 +0100

    la hauteur du drawingboard est adaptée au viewport

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 (function() {
-	if ($('#teuboard').length)
+	var $teuboard = $("#teuboard");
+	if ($teuboard.length) {
+		$teuboard.addClass('hidden');
+		$teuboard.height(
+			$(window).height() - $('#container').outerHeight()
+		);
+		$teuboard.removeClass('hidden');
 		var board = new DrawingBoard.Board('teuboard', {
 			controls: [{ Color: {defaultColor: "rgba(255, 191, 127, 1)"}}, 'Size', 'Navigation']
 		});
+	}
+
 	$('.teu').each(function(key, item) {
 		console.log(item.innerHTML);
 		item.innerHTML = item.innerHTML.replace(/teube/, 'teu<span class="be">be</span>');
