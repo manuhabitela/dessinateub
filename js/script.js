dfabe519cca62e20fe83b66c92e890a4c347a95b
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-04-10 21:09:11 +0200

    le canvas a une taille fixe en fait, pas suivant l'écran

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 (function() {
-	var $teuboard = $("#teuboard");
-	if ($teuboard.length) {
-		$teuboard.addClass('hidden');
-		$teuboard.height(
-			$(window).height() - $('#container').outerHeight()
-		);
-		$teuboard.removeClass('hidden');
-		window.teuboard = new DrawingBoard.Board('teuboard', {
-			color: "rgba(255, 191, 127, 1)",
-			size: 5
-		});
-	}
+	window.teuboard = new DrawingBoard.Board('teuboard', {
+		color: "rgba(255, 191, 127, 1)",
+		size: 5,
+		localStorage: true
+	});
 
 	$('.teu').each(function(key, item) {
 		item.innerHTML = item.innerHTML.replace(/teube(s|)/, 'teu<span class="pointbe">.</span>b<span class="pointbe">e</span>$1');
