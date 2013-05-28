e450a1b43f5dfbbbafb30f1a09d0a13e9c850ba1
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-05-28 23:02:00 +0200

    maj du js (deal with it toussa)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
 (function() {
 	$('.teu').each(function(key, item) {
-		item.innerHTML = item.innerHTML.replace(/teube(s|)/, 'teu<span class="pointbe">.</span>b<span class="pointbe">e</span>$1');
+		item.innerHTML = item.innerHTML.replace(/teub(s|)/, 'teu<span class="pointbe">.</span>b<span class="pointbe">e</span>$1');
 	});
 
-	/**
-	 * page de dessin de teube
-	 */
-	window.teuboard = new DrawingBoard.Board('teuboard', {
-		color: "rgba(255, 191, 127, 1)",
-		size: 5,
-		localStorage: true,
-		errorMessage: "<p>Votre navigateur est obsolète : mettez-le à jour pour pouvoir dessiner des teubs.</p>"
-	});
 
-	var existingImg = $('.teube-view') ? $('.teube-view').attr('data-url') : null;
-	if (existingImg) {
-		teuboard.setImg(existingImg);
+	if ($('html.draw').length) {
+		window.teuboard = new DrawingBoard.Board('teuboard', {
+			color: "rgba(255, 191, 127, 1)",
+			size: 5,
+			localStorage: true,
+			errorMessage: "<p>Votre navigateur est obsolète : mettez-le à jour pour pouvoir dessiner des teubs.</p>"
+		});
+		var submitted = false;
+		window.onbeforeunload = function() {
+			if (!submitted && typeof window.teuboard.blankCanvas !== "undefined" && window.teuboard.blankCanvas !== window.teuboard.getImg())
+				return "Attention : votre teube disparaitra à jamais si vous continuez.";
+			else
+				return;
+		};
+
+		$('.teube-add').on('submit', function(e) {
+			var img = teuboard.getImg();
+			var imgInput = (teuboard.blankCanvas == img) ? '' : img;
+			$(this).find('input[name=image]').val( img );
+			submitted = true;
+		});
 	}
 
-	$('.teube-add').on('submit', function(e) {
-		$(this).find('input[name=image]').val( teuboard.getImg() );
-	});
+	if ($('html.view').length) {
+		window.teuboard = new DrawingBoard.Board('teuboardz', {
+			color: "rgba(255, 191, 127, 1)",
+			size: 5,
+			localStorage: false,
+			errorMessage: "<p>Votre navigateur est obsolète : mettez-le à jour pour pouvoir dessiner des teubs.</p>"
+		});
+		var existingImg = $('.teube-view') ? $('.teube-view').attr('data-url') : null;
+		if (existingImg) {
+			teuboard.setImg(existingImg);
+		}
+		$('#teube-url').on('click', function(e) { $(this).select(); });
+	}
 })();
