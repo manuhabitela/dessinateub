ea7db8a3bed25b0f0f81443d9ce0e022169e1e2e
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-05-28 22:42:22 +0200

    homepage toute simple, toute flat et tout

--- views/home.php
+++ views/home.php
@@ -1,4 +1,16 @@
 <?php if (!$app->request()->isAjax()) include(__DIR__ . '/layout/head.php'); ?>
 
+<div class="home-part home-draw">
+	<h1><a href="<?php echo $app->urlFor('draw'); ?>" class="teu bouton">Je dessine ma teub</a></h1>
+</div>
+
+<div class="home-part home-galery">
+	<ul class="hidden verbs">
+		<li>Je mate</li>
+		<li>Je vote pour</li>
+		<li>J'admire</li>
+	</ul>
+	<h1><a href="<?php echo $app->urlFor('teubes'); ?>" class="teu bouton"><span class="verb">Je mate</span> les teubs</a></h1>
+</div>
 
 <?php if (!$app->request()->isAjax()) include(__DIR__ . '/layout/foot.php'); ?>
