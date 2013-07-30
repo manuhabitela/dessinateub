11e7ff70978ea3f855eec56321d0796955562f7c
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-07-30 20:24:36 +0200

    drawingboard: pas de localstorage

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,21 @@
+//méthode rache mothafucka
 (function() {
 	$('.teu').each(function(key, item) {
 		item.innerHTML = item.innerHTML.replace(/teub(s|)/, 'teu<span class="pointbe">.</span>b<span class="pointbe">e</span>$1');
 	});
 
 
+	//page de dessin d'une teube
 	if ($('html.draw').length) {
 		window.teuboard = new DrawingBoard.Board('teuboard', {
 			color: "rgba(255, 191, 127, 1)",
 			size: 5,
-			localStorage: true,
 			errorMessage: "<p>Votre navigateur est obsolète : mettez-le à jour pour pouvoir dessiner des teubs.</p>"
 		});
+		var initialDrawing = window.teuboard.getImg();
 		var submitted = false;
 		window.onbeforeunload = function() {
-			if (!submitted && typeof window.teuboard.blankCanvas !== "undefined" && window.teuboard.blankCanvas !== window.teuboard.getImg())
+			if (!submitted && initialDrawing !== window.teuboard.getImg())
 				return "Attention : votre teube disparaitra à jamais si vous continuez.";
 			else
 				return;
@@ -27,6 +29,7 @@
 		});
 	}
 
+	//page de vue d'une teube
 	if ($('html.view').length) {
 		window.teuboard = new DrawingBoard.Board('teuboardz', {
 			color: "rgba(255, 191, 127, 1)",
