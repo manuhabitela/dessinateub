725038c2f821a6eff701ea1c261f9bdd7f73c151
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-08-11 12:32:23 +0200

    images de teu: meilleure gestion de la créa des fichiers on fait tout dans le model dans les callbacks de save/delete

--- app/lib/halp.php
+++ app/lib/halp.php
@@ -1,6 +1,16 @@
-<?php 
+<?php
 class Halp {
-	public static function drawing($teube) {
-		return "/drawings/".$teube->id.".png";
+	/**
+	 * Retourne l'image liée à une teube
+	 *
+	 * @param  Bean $teube objet teube de la base
+	 * @param  boolean $dataURIFallback si aucune image est trouvée on peut choisir de retourner l'image encodée en base64 contenu dans la $teube
+	 * @return string chemin ou data URI de l'image
+	 */
+	public static function drawing($teube, $dataURIFallback = false) {
+		$file = "/drawings/".$teube->id.".png";
+		if ($dataURIFallback && !file_exists(WEBROOT_PATH.$file) && !empty($teube->image))
+			$file = $teube->image;
+		return $file;
 	}
 }
--- app/models/teube.php
+++ app/models/teube.php
@@ -1,21 +1,36 @@
 <?php
+use RedBean_Facade as R;
 class Model_Teube extends RedBean_SimpleModel
 {
-	protected $folder;
+	public function open() {
 
-	public function __construct() {
-		$this->folder = WEBROOT_PATH.'/drawings/';
+		if (!file_exists($this->getDrawingPath()))
+			$this->createDrawingFile();
+	}
+
 	}
 
 	public function after_update() {
-		//http://j-query.blogspot.fr/2011/02/save-base64-encoded-canvas-image-to-png.html
-		$img = str_replace(' ', '+', str_replace('data:image/png;base64,', '', $this->bean->image));
-		$data = base64_decode($img);
-		$file = $this->folder . $this->bean->id . '.png';
-		file_put_contents($file, $data);
+		$this->createDrawingFile();
 	}
 
 	public function after_delete() {
-		unlink($this->folder . $this->bean->id . '.png');
+		$this->deleteDrawingFile();
+	}
+
+	public function getDrawingPath() {
+		return WEBROOT_PATH.'/drawings/'.$this->id.'.png';
+	}
+
+	public function createDrawingFile() {
+		if (empty($this->image)) return false;
+		//http://j-query.blogspot.fr/2011/02/save-base64-encoded-canvas-image-to-png.html
+		$img = str_replace(' ', '+', str_replace('data:image/png;base64,', '', $this->image));
+		$data = base64_decode($img);
+		return file_put_contents($this->getDrawingPath(), $data);
+	}
+
+	public function deleteDrawingFile() {
+		return unlink($this->getDrawingPath());
 	}
 }
