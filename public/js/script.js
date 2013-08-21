2417458ca7be8426407eba630542b2e2f70a9748
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-08-22 00:29:53 +0200

    piwik!

--- public/js/script.js
+++ public/js/script.js
@@ -141,4 +141,18 @@
 		return (url.toLowerCase() === (window.location.pathname + window.location.search).toLowerCase()) ||
 				(url.toLowerCase() === window.location.href.toLowerCase());
 	}
+
+	//piwiiiiiik
+	var _paq = _paq || [];
+	_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+	_paq.push(["setCookieDomain", "*.jaiunegrosseteu.be"]);
+	_paq.push(["trackPageView"]);
+	_paq.push(["enableLinkTracking"]);
+	(function() {
+		var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.gafm.fr/";
+		_paq.push(["setTrackerUrl", u+"piwik.php"]);
+		_paq.push(["setSiteId", "10"]);
+		var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+		g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+	})();
 })();
