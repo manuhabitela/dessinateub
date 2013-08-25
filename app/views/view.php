bb6033d7559c9fe115ccc2d667c1236075619641
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-08-25 19:46:10 +0200

    vue d'une teu : style des boutons d'édition/suppression

--- app/views/view.php
+++ app/views/view.php
@@ -2,59 +2,57 @@
 
 <div class="teube-view" data-id="<?php echo $teube->id ?>" data-url="<?php echo Halp::drawing($teube, true) ?>" data-rating="<?php echo round($teube->w_rating) ?>">
 
-	<h2 class="teube-view__name"><?php echo $teube->name ?></h2>
-
-	<?php if ($isEditable): ?>
-	<div class="teube-view__user-actions">
-		<a class="teube-view__edit-link button" href="<?php echo $app->urlFor('draw-edit', array('slug' => $teube->id)) ?>">Modifier cette teube</a>
-		<form action="<?php echo $app->urlFor('draw-delete', array('slug' => $teube->id)) ?>" method="post">
-			<input type="hidden" name="_METHOD" value="DELETE">
-			<button class="teube-view__delete-link button">Supprimer cette teube</button>
-		</form>
-	</div>
-	<?php endif ?>
-
-	<div class="teube-view__content">
-
-		<p class="teube-view__info">
-			<?php $teubeTimestamp = strtotime($teube->created); ?>
-			<?php $teubeDate = date("d/m/Y à H:i", $teubeTimestamp); ?>
-			Œuvre réalisée
-			par <span class="teube-view__artist"><?php echo $teube->artist ? $teube->artist : 'un inconnu' ?></span>
-			<span class="teube-view__date" data-timestamp="<?php echo $teubeTimestamp*1000 ?>" title="Le <?php echo $teubeDate ?>">le <?php echo strstr($teubeDate, 'à', true); ?></span>
-		</p>
-
-		<div class="teube-view__vote-container">
-			<?php if (isset($userVote)): ?>
-			<span class="teube-view__user-vote">votre note : <span><?php echo $userVote->value ?></span></span>
-			<?php endif ?>
-			<form action="#" class="teube-view__vote cf">
-				<input type="hidden" name="teube-id" value="<?php echo $teube->id ?>">
-				<fieldset>
-					<input type="radio" id="teube-vote-5" name="teube-vote" value="5" /><label for="teube-vote-5" title="C'est parfait ! Maintenant j'ai besoin d'un slip de rechange.">5</label>
-					<input type="radio" id="teube-vote-4" name="teube-vote" value="4" /><label for="teube-vote-4" title="J'aimerais avoir la même à la maison !">4</label>
-					<input type="radio" id="teube-vote-3" name="teube-vote" value="3" /><label for="teube-vote-3" title="J'ai une demi-molle bien entamée.">3</label>
-					<input type="radio" id="teube-vote-2" name="teube-vote" value="2" /><label for="teube-vote-2" title="C'est moche mais un peu excitant.">2</label>
-					<input type="radio" id="teube-vote-1" name="teube-vote" value="1" /><label for="teube-vote-1" title="Non. NON.">1</label>
-				</fieldset>
+	<h2 class="teube-view__name">
+		<?php echo $teube->name ?>
+		<?php if ($isEditable): ?>
+		<div class="teube-view__user-actions">
+			<a data-icon-before="p" class="teube-view__edit-link button" href="<?php echo $app->urlFor('draw-edit', array('slug' => $teube->id)) ?>" title="Modifier cette teub"></a>
+			<form action="<?php echo $app->urlFor('draw-delete', array('slug' => $teube->id)) ?>" method="post">
+				<input type="hidden" name="_METHOD" value="DELETE">
+				<button data-icon-before="t" class="teube-view__delete-link button" title="Supprimer cette teub"></button>
 			</form>
-			<span class="teube-view__avg-vote">moyenne : <?php echo $teube->w_rating ?> avec <?php echo $teube->ratings_count.' '.Halp::pluralize('vote', $teube->ratings_count); ?></span>
 		</div>
+		<?php endif ?>
+	</h2>
 
-		<img class="teube-view__drawing" src="<?php echo Halp::drawing($teube, true) ?>">
+	<p class="teube-view__info">
+		<?php $teubeTimestamp = strtotime($teube->created); ?>
+		<?php $teubeDate = date("d/m/Y à H:i", $teubeTimestamp); ?>
+		Œuvre réalisée
+		par <span class="teube-view__artist"><?php echo $teube->artist ? $teube->artist : 'un inconnu' ?></span>
+		<span class="teube-view__date" data-timestamp="<?php echo $teubeTimestamp*1000 ?>" title="Le <?php echo $teubeDate ?>">le <?php echo strstr($teubeDate, 'à', true); ?></span>
+	</p>
 
-		<div class="teube-view__share-container">
-			<span class="teu">Partager sur </span>
-			<!-- <a class="twitter teube-button" href="https://twitter.com/intent/tweet?text=" target="_blank">Twitter</a> -->
-			<a class="condom--twitter condom--expandable teube-view__share-link" href="#" target="_blank">Twitter</a>
-			<!-- <a class="facebook teube-button" href="http://www.facebook.com/sharer/sharer.php?u=&amp;t=jaiunegrosseteu.be" target="_blank">Facebook</a> -->
-			<a class="condom--facebook condom--expandable teube-view__share-link" href="#" target="_blank">Facebook</a>
-		</div>
+	<div class="teube-view__vote-container">
+		<?php if (isset($userVote)): ?>
+		<span class="teube-view__user-vote">votre note : <span><?php echo $userVote->value ?></span></span>
+		<?php endif ?>
+		<form action="#" class="teube-view__vote cf">
+			<input type="hidden" name="teube-id" value="<?php echo $teube->id ?>">
+			<fieldset>
+				<input type="radio" id="teube-vote-5" name="teube-vote" value="5" /><label for="teube-vote-5" title="C'est parfait ! Maintenant j'ai besoin d'un slip de rechange.">5</label>
+				<input type="radio" id="teube-vote-4" name="teube-vote" value="4" /><label for="teube-vote-4" title="J'aimerais avoir la même à la maison !">4</label>
+				<input type="radio" id="teube-vote-3" name="teube-vote" value="3" /><label for="teube-vote-3" title="J'ai une demi-molle bien entamée.">3</label>
+				<input type="radio" id="teube-vote-2" name="teube-vote" value="2" /><label for="teube-vote-2" title="C'est moche mais un peu excitant.">2</label>
+				<input type="radio" id="teube-vote-1" name="teube-vote" value="1" /><label for="teube-vote-1" title="Non. NON.">1</label>
+			</fieldset>
+		</form>
+		<span class="teube-view__avg-vote">moyenne : <?php echo $teube->w_rating ?> avec <?php echo $teube->ratings_count.' '.Halp::pluralize('vote', $teube->ratings_count); ?></span>
+	</div>
 
-		<div class="disqus-container">
-			<div id="disqus_thread">
+	<img class="teube-view__drawing" src="<?php echo Halp::drawing($teube, true) ?>">
 
-			</div>
+	<div class="teube-view__share-container">
+		<span class="teu">Partager sur </span>
+		<!-- <a class="twitter teube-button" href="https://twitter.com/intent/tweet?text=" target="_blank">Twitter</a> -->
+		<a class="condom--twitter condom--expandable teube-view__share-link" href="#" target="_blank">Twitter</a>
+		<!-- <a class="facebook teube-button" href="http://www.facebook.com/sharer/sharer.php?u=&amp;t=jaiunegrosseteu.be" target="_blank">Facebook</a> -->
+		<a class="condom--facebook condom--expandable teube-view__share-link" href="#" target="_blank">Facebook</a>
+	</div>
+
+	<div class="disqus-container">
+		<div id="disqus_thread">
+
 		</div>
 	</div>
 </div>
