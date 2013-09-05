52589402b4eeebbb85782262ec0dca5833ab6725
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-09-05 15:49:52 +0200

    osef

--- app/routes/updates.php
+++ app/routes/updates.php
@@ -2,19 +2,19 @@
 use RedBean_Facade as R;
 
 //surement obsolète
-$app->get('/update-comments', function() use($app) {
-	$posts = json_decode(file_get_contents("http://disqus.com/api/3.0/threads/list.json?api_key=" . APP_DISQUS_API . "&forum=" . APP_DISQUS_NAME), true);
-	foreach ($posts['response'] as $post) {
-		if ($post['isDeleted'])
-			continue;
-		$id = substr(strrchr($post['link'], '/'), 1);
-		$teube = R::load('teube', $id);
-		if (!empty($teube->id)) {
-			$teube->comments_count = $post['posts'];
-			R::store($teube);
-		}
-	}
-});
+// $app->get('/update-comments', function() use($app) {
+// 	$posts = json_decode(file_get_contents("http://disqus.com/api/3.0/threads/list.json?api_key=" . APP_DISQUS_API . "&forum=" . APP_DISQUS_NAME), true);
+// 	foreach ($posts['response'] as $post) {
+// 		if ($post['isDeleted'])
+// 			continue;
+// 		$id = substr(strrchr($post['link'], '/'), 1);
+// 		$teube = R::load('teube', $id);
+// 		if (!empty($teube->id)) {
+// 			$teube->comments_count = $post['posts'];
+// 			R::store($teube);
+// 		}
+// 	}
+// });
 
 $app->get('/update-votes', function() use($app) {
 	$teubes = R::findAll('teube');
@@ -39,9 +39,9 @@ $app->get('/update-pageviews/:slug', function($slug) use($app) {
 	$teube->updatePageViews();
 });
 
-$app->get('/update-pageviews', function() use($app) {
-	$teubes = R::findAll('teube');
-	foreach ($teubes as $teube) {
-		$teube->updatePageViews();
-	}
-});
+// $app->get('/update-pageviews', function() use($app) {
+// 	$teubes = R::findAll('teube');
+// 	foreach ($teubes as $teube) {
+// 		$teube->updatePageViews();
+// 	}
+// });
